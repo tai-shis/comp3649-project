b6f6da738224d1d5a404b82175153b216f58f48d
tai-shis <tshis070@mtroyal.ca>
2026-01-25 17:07:56 -0700

    move instruction class to instruction_buffer

--- imperative/input/instruction_buffer.py
+++ imperative/input/instruction_buffer.py
@@ -1,6 +1,22 @@
 from llist import dllist, dllistnode
-from instruction import Instruction
             
+class Instruction:
+    def __init__(self, type: int, dest: str, operand1: str=None, operator: str=None, operand2: str=None):
+        self.type: int = type
+        self.dest: str = dest
+        self.operand1: str = operand1
+        self.operator: str = operator
+        self.operand2: str = operand2
+
+    def __str__(self):
+        match self.type:
+            case 0:
+                return f"{self.dest} = {self.operand1} {self.operator} {self.operand2}"
+            case 1:
+                return f"{self.dest} = {self.operator}{self.operand2}"
+            case 2:
+                return f"{self.dest} = {self.operand1}"
+
 class InstructionBuffer:
     def __init__(self):
         self.instructions: dllist[Instruction] = dllist()
