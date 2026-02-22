d046d2b57eb360a62fabb19c64db6cd4e67721a9
TyW04 <tywagner04@gmail.com>
2026-02-22 14:04:47 -0700

    Small logic error in binary operator asm generation

--- imperative/generator/asm_generator.py
+++ imperative/generator/asm_generator.py
@@ -1,50 +1,3 @@
-'''
-- Traverse InstructionBuffer to build corressponding assembly language sequence
-- Each three-address instruction will corresspond to one or two ASM language instructions.
-    - Register assignments from graph colouring will assist in this as well as later optimizations
-
-ASM OpCodes: [ADD src,Ri, SUB src,Ri, MUL src,Ri, DIV src,Ri, MOV src,Ri, MOV Ri,dst]
-    - Where Ri is a register 0 <= i < n (n is number of registers available)
-    - ADD src,Ri -> adds src operand to the contents of Ri and stores the sum in Ri
-    - SUB src,Ri -> subtracts the src operand from the contents of Ri and stores the difference in Ri
-    - MUL src,Ri -> multiplies the src operand by the contents of Ri and stores the product in Ri
-    - DIV src,Ri -> divides the contents of Ri by the src operand and stores the quotient in Ri
-    - MOV src,Ri -> copies the contents of src into Ri
-    - MOV Ri,dst -> copies the contents of Ri into dst
-There are modes for src:
-    - immediate mode: #x -> x is an integer literal
-    - absolute mode: x -> x is the name of a variable
-    - register direct mode: Ri -> Ri is a register as seen above
-There are modes for dst:
-    - absolute mode: x -> x is the name of a variable
-    - register direct mode: Ri -> Ri is a register as seen above
-
-Example:
-
-Three-address-instructions
-    a = a + 1
-    t1 = a * 4
-    t2 = t1 + 1
-    b = t2 - t1
-
-ASM code
-    MOV a,R0 ; Allocate a to R0
-    ADD #1,R0
-    MOV R0,R1 ; Allocate t1 to R1
-    MUL #4,R1 
-    MOV R1,R2 ; Allocate t2 to R2 
-    ADD #1,R2
-    MOV R2,R5 ; R5 is allocated to b
-    SUB R1,R2 ; Subtract t1 from t2
-
-So if we traverse through the InstructionBuffer with the type="binary_operator", we know we are probably going to have to use MOV at least once,
-some arithmetic operator like MUL, ADD, SUB, DIV, and then possibly another MOV.
-If we see something like type="assignment", we just going to be using a MOV
-
-This can be deleted anytime really. Just put this here as I started so I don't have to
-reference D2L
-'''
-
 from intermediate.interference_graph import InterferenceGraph
 from input.instruction_buffer import InstructionBuffer
 from input.instruction import Instruction
@@ -116,9 +69,9 @@ class ASMGenerator:
         match instruction.type:
             case 0: # Binary Operator
                 
-                dest = self._get_register(instruction.dest)
-                op1 = self._get_register(instruction.operand1)
-                op2 = self._get_register(instruction.operand2)
+                dest = self._get_reg_or_value(instruction.dest)
+                op1 = instruction.operand1.value
+                op2 = self._get_reg_or_value(instruction.operand2)
                 op_code = self._get_op_code(instruction.operator)
                 
                 operation1 = f"MOV {op1},{dest}"
@@ -132,8 +85,8 @@ class ASMGenerator:
                 # MOV R0, R1 ; Storing x in R1
                 # MUL #-1,R1 ; taking inverse of b and storing in x (value in R0)
 
-                dest = self._get_register(instruction.dest)
-                source = self._get_register(instruction.operand2)
+                dest = self._get_reg_or_value(instruction.dest)
+                source = self._get_reg_or_value(instruction.operand2)
                 op_symbol = instruction.operator.value
 
                 if (op_symbol == '-'):
@@ -151,8 +104,8 @@ class ASMGenerator:
                 
             case 2: # Assignment
                 # In this case the operator will always be a MOV
-                dest = self._get_register(instruction.dest)
-                source = self._get_register(instruction.operand1)
+                dest = self._get_reg_or_value(instruction.dest)
+                source = self._get_reg_or_value(instruction.operand1)
                 return "MOV" + ' ' + source + ',' + dest
             
     def generate_assembly(self) -> list[str]:
