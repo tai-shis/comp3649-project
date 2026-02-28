9d2dbe5b6b743b1fb673035e3f78fc1b6dd330e6
Ty Wagner <tywagner04@gmail.com>
2026-02-27 20:18:10 -0700

    Was using _init_ instead of __init__. Fixed that.

--- imperative/generator/asm_instruction.py
+++ imperative/generator/asm_instruction.py
@@ -1,22 +1,11 @@
 class ASMInstruction:
-    def _init_(self, op_code: str, op1: str, op2: str):
+    def __init__(self, op_code: str, op1: str, op2: str):
         '''
-        :param op_code: op_code as a single character ('+', '-', '*', '/', 'm')
+        :param op_code: op_code as a single character
         :param op1: source (or register if MOV is being used) operand
         :param op2: destination variable or register
         '''
 
-        if (len(op_code) > 1):
-            ValueError("op_code must only be one character ('+', '-', '*', '/', 'm')")
-
-        self.op_code = self.opcodes[op_code]
+        self.op_code = op_code
         self.op1 = op1
         self.op2 = op2
-
-        self.opcodes = {
-            '+': 'ADD',
-            '-': 'SUB',
-            '*': 'MUL',
-            '/': 'DIV',
-            'm': 'MOV'
-        }
