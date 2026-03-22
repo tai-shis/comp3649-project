980096df3769dc36966dc912a5abd40aae35d566
Ty Wagner <tywagner04@gmail.com>
2026-03-22 16:58:40 -0600

    Representation for an Instruction

---
+++ haskell/input/instruction.hs
@@ -0,0 +1,18 @@
+import Token
+
+data InstructionType 
+    = Invalid
+    | BinaryOperator
+    | UnaryOperator
+    | Assignment
+    deriving (Show, Eq)
+
+data Dest = Token
+data Operand = Token
+data Operator = Token
+
+data Instruction 
+    = BinaryIns InstructionType Dest Operand Operator Operand
+    | UnaryIns InstructionType Dest Operator Operand
+    | Assignment = InstructionType Dest Operand
+    deriving (Show, Eq)
