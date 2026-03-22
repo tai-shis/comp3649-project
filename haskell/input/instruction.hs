f87bf37142918de5a07d29873c993b681cdc586b
Ty Wagner <tywagner04@gmail.com>
2026-03-22 17:44:58 -0600

    getVariables function with helpers. Untested

--- haskell/input/instruction.hs
+++ haskell/input/instruction.hs
@@ -16,3 +16,24 @@ data Instruction
     | UnaryIns InstructionType Dest Operator Operand
     | AssignmentIns = InstructionType Dest Operand
     deriving (Show, Eq)
+
+getInstructionDest :: Instruction -> Token
+getInstructionDest (Instruction _ dest _ _ _) = dest
+
+getInstructionOperand :: Int -> Instruction -> Token
+getInstructionOperand operandNum (Instruction type _ op1 _ op2) = 
+    case type of
+        BinaryOperator ->
+            if (operandNum == 1)
+                op1
+            else
+                op2
+        UnaryOperator -> op2
+        Assignment -> op1
+
+-- Retrieves all variable tokens stored in the given instruction
+getListVariables :: Instruction -> [Token]
+getListVariables instruction = 
+    [getInstructionDest instruction,
+    getInstructionOperand (1 instruction),
+    getInstructionOperand (2 instruction)]
