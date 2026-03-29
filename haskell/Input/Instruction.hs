5c72fa7e38f9b69ef51406bd962e9162a271ba55
TyW04 <tywagner04@gmail.com>
2026-03-28 21:22:37 -0600

    Modified show functions and module exports to fix compile errors

--- haskell/Input/Instruction.hs
+++ haskell/Input/Instruction.hs
@@ -1,4 +1,8 @@
-module Instruction (Instruction, Instructions, getVariables) where
+module Instruction (Instruction,
+                    Instructions,
+                    getVariables,
+                    showInstructions,
+                    showLiveVars) where
 
 import Token
 
@@ -19,28 +23,29 @@ data Instruction
     | AssignmentIns InstructionType Dest Operand
     deriving (Eq)
 
+data LiveVariable = LiveVar String
+
+-- Doesn't have to be a pair
+data Instructions = Instruction ([Instruction],[LiveVariable])
+
 instance Show Instruction where
     show (BinaryIns _ dest op1 operator op2) = "Binary Instruction: " ++ show dest ++ " = " ++ show op1 ++ " " ++ show operator ++ " " ++ show op2
     show (UnaryIns _ dest operator op) = "Unary Instruction: " ++ show dest ++ " = " ++ show operator ++ " " ++ show op
     show (AssignmentIns _ dest op) = "Assignment Instruction: " ++ show dest ++ " = " ++ show op
 
-instance Show [Instruction] where
-    show instructions = "Instructions: \n" ++ map (\x -> show x ++ "\n") instructions
-
-data LiveVariable = Live String
-
 instance Show LiveVariable where
-    show (Live var) = var ++ ","
-
-instance Show [LiveVariable] where
-    show liveVars = "Live: " ++ map show liveVars
-
--- Doesn't have to be a pair
-data Instructions
-    = Instruction ([Instruction],[LiveVariable])
+    show (LiveVar var) = var ++ ","
 
 instance Show Instructions where
-    show (Instruction (ins,live)) = show ins ++ show live
+    show (Instruction (ins,live)) = showInstructions ins ++ showLiveVars live
+
+-- Public: Prints all instructions
+showInstructions :: [Instruction] -> String
+showInstructions instructions = "Instructions: \n" ++ concatMap (\x -> show x ++ "\n") instructions
+
+-- Public: Prints all live variables
+showLiveVars :: [LiveVariable] -> String
+showLiveVars liveVars =  "Live: " ++ concatMap show liveVars
 
 -- Public: Retrieves all variable tokens stored in the given instruction
 getVariables :: Instruction -> [Token]
