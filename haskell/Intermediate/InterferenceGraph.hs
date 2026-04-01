a712b1cb75ded3cc5c873d4e2d5a10bc87003a0b
tai-shis <tshis070@mtroyal.ca>
2026-04-01 16:01:58 -0600

    feat: liveness implemented

--- haskell/Intermediate/InterferenceGraph.hs
+++ haskell/Intermediate/InterferenceGraph.hs
@@ -9,6 +9,9 @@ module Intermediate.InterferenceGraph (
     addEdge
 ) where
 
+import Lib.Helper (
+    commaSperatedList, 
+    pairTrue)
 
 import Data.Set (
     Set, 
@@ -16,14 +19,26 @@ import Data.Set (
     empty, 
     insert)
 
+import Intermediate.Liveness (
+    LivenessState,
+    LivenessStates,
+    showLivenessStates,
+    getLivenessName,
+    namesFromLiveness,
+    isLive,
+    isDefined,
+    isUnlive,
+    determineLiveness,
+    livenessInfo)
+
+-- for tests
 import Input.Token (
     Token, 
     TokenType(
         Destination,
         Variable,
         Literal,
-        Operator
-    ), 
+        Operator),
     getType, 
     getValue,
     createToken)
@@ -32,17 +47,14 @@ import Input.Instruction (
     Instruction, 
     Instructions,
     getVariables, 
+    getDestination,
     getInstructions,
     getLiveVariables,
     createInstruction, 
     fromArraysInstructions)
 
-import Lib.Helper (
-    commaSperatedList, 
-    addList)
 
 type Neighbors = Set String
-
 
 -- Variables name, and their neighbors (just the names)
 data Variable = Var (String, Neighbors)
@@ -94,14 +106,12 @@ edgeExists (v:vs) name1 name2 | name1 == name2 = (False, False)
         where rest = edgeExists vs name1 name2
               name = getName v
 
--- Private: And on a pair of bools
-pairTrue :: (Bool, Bool) -> Bool
-pairTrue (b1, b2) = b1 && b2
-
 -- Public: Adds an edge between two variables in the graph if they both exist, otherwise returns the original graph
 addEdge :: Graph -> String -> String -> Graph
 addEdge graph name1 name2 | pairTrue (edgeExists (getVertices graph) name1 name2) = Graph (map (updateNeighbors name1 name2) (getVertices graph))
     | otherwise = graph
+
+
 
 -- TEST DATA
 ins1 = createInstruction (createToken "a"  Destination, createToken "a"  Variable, createToken "+"  Operator, createToken "1"  Literal)
@@ -113,68 +123,5 @@ ins6 = createInstruction (createToken "t4" Destination, createToken "b"  Variabl
 ins7 = createInstruction (createToken "d"  Destination, createToken "c"  Variable, createToken "+"  Operator, createToken "t5" Variable)
 
 is = fromArraysInstructions [ins1, ins2, ins3, ins4, ins5, ins6, ins7] ["d"]
-
--- Liveness analysis 
-type LivenessStates = [LivenessState]
-
-data LivenessState = Defined String 
-                   | Live String
-
-instance Eq LivenessState where
-    (Defined name1) == (Defined name2) = name1 == name2
-    (Live name1) == (Live name2) = name1 == name2
-    (Defined name1) == (Live name2) = name1 == name2
-    (Live name1) == (Defined name2) = name1 == name2
-
-instance Show LivenessState where
-    show (Defined name) = name ++ ": defined"
-    show (Live name) = name ++ ": live"
-
-getLivenessName :: LivenessState -> String
-getLivenessName (Defined name) = name
-getLivenessName (Live name) = name
-
--- Public: Takes a list of liveness and returns a list of the variables.
-namesFromLiveness :: LivenessStates -> [String]
-namesFromLiveness = map getLivenessName
-
--- Private: Checks if a liveness state is live or defined
-isLive :: LivenessState -> Bool
-isLive (Live _) = True
-isLive _ = False
-
--- Private: Checks if a liveness state is live or defined
-isDefined :: LivenessState -> Bool
-isDefined (Defined _) = True
-isDefined _ = False
-
--- Private: Convert our end of line "live: " variables into our carry variables
-initialLiveness :: Instructions -> LivenessStates
-initialLiveness instructions = map (\x -> Live x) (getLiveVariables instructions)
-
--- Private: Strip the defined variables from the previous liveness state.
-stripDefined :: LivenessStates -> LivenessStates
-stripDefined liveness = filter isLive liveness
-
--- Private: Split instruction into the destination and variables, our tail variables are always live 
-splitInstruction :: [Token] -> (String, LivenessStates)
-splitInstruction [] = ("", [])  -- No destination will never happen.
-splitInstruction (dest:vars) = (getValue dest, map (\x -> Live (getValue x)) vars) 
-
--- Private: Given an instruction and previous liveness, generates the new liveness state
-markLiveness :: LivenessStates -> String -> LivenessStates -> LivenessStates
-markLiveness prevLiveness dest variables | elem dest (namesFromLiveness variables) = addList variables (Live dest : prevLiveness)
-    | otherwise = addList variables (Defined dest : prevLiveness)
-
--- Private: Given a list of instructions and an initial liveness state, generates a list of liveness states for each instruction
-determineRestLiveness :: [Instruction] -> LivenessStates -> [LivenessStates]
-determineRestLiveness [] liveness = []
-determineRestLiveness (instruction:instructions) liveness = (determineRestLiveness instructions newLiveness)
-    where (dest, variables) = splitInstruction (getVariables instruction)
-          previousLiveness = stripDefined liveness 
-          newLiveness = markLiveness previousLiveness dest variables
-
--- Public: Determines the liveness of each instruction in the given Instructions and returns a list of liveness states for each instruction
-determineLiveness :: Instructions -> [LivenessStates]
-determineLiveness instructions = determineRestLiveness (getInstructions instructions) (initialLiveness instructions)
-
+liveness = determineLiveness is
+linfo = livenessInfo liveness
