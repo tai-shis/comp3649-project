a3a0244f0b18735bca0a74a00d75c5222ff5360f
Ty Wagner <tywagner04@gmail.com>
2026-04-02 21:51:58 -0600

    small syntax update

--- imperative/tests/instruction_test.py
+++ imperative/tests/instruction_test.py
@@ -6,7 +6,7 @@ from input.instruction import Instruction
 class TestInstruction(unittest.TestCase):
 
     def test_binary_operator_construction(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operand1 = Token("a", 1)
         operator = Token("+", 3)
         operand2 = Token("b", 1)
@@ -19,7 +19,7 @@ class TestInstruction(unittest.TestCase):
         self.assertEqual(instr.operand2, operand2)
 
     def test_unary_operator_construction(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operator = Token("-", 3)
         operand2 = Token("a", 1)
         instr = Instruction(1, dest, operator=operator, operand2=operand2)
@@ -31,7 +31,7 @@ class TestInstruction(unittest.TestCase):
         self.assertIsNone(instr.operand1)
 
     def test_assignment_construction(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operand1 = Token("a", 1)
         instr = Instruction(2, dest, operand1)
 
@@ -42,7 +42,7 @@ class TestInstruction(unittest.TestCase):
         self.assertIsNone(instr.operand2)
 
     def test_invalid_type_construction(self):
-        dest  = Token("x", 0)
+        dest = Token("x", 0)
         instr = Instruction(-1, dest)
 
         self.assertEqual(instr.type, -1)
@@ -64,7 +64,7 @@ class TestInstruction(unittest.TestCase):
         self.assertEqual(str(instr), "x = 42")
 
     def test_binary_all_variables(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operand1 = Token("a", 1)
         operand2 = Token("b", 1)
         instr = Instruction(0, dest, operand1, Token("+", 3), operand2)
@@ -73,7 +73,7 @@ class TestInstruction(unittest.TestCase):
         self.assertEqual(result, [dest, operand1, operand2])
 
     def test_binary_literal_operands(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operand1 = Token("1", 2)
         operand2 = Token("2", 2)
         instr = Instruction(0, dest, operand1, Token("+", 3), operand2)
@@ -82,7 +82,7 @@ class TestInstruction(unittest.TestCase):
         self.assertEqual(result, [dest])
 
     def test_binary_mixed_operands(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operand1 = Token("a", 1)
         operand2 = Token("2", 2)
         instr = Instruction(0, dest, operand1, Token("+", 3), operand2)
@@ -91,7 +91,7 @@ class TestInstruction(unittest.TestCase):
         self.assertEqual(result, [dest, operand1])
 
     def test_unary_variable_operand(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operand2 = Token("a", 1)
         instr = Instruction(1, dest, operator=Token("-", 3), operand2=operand2)
 
@@ -99,7 +99,7 @@ class TestInstruction(unittest.TestCase):
         self.assertEqual(result, [dest, operand2])
 
     def test_unary_literal_operand(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operand2 = Token("42", 2)
         instr = Instruction(1, dest, operator=Token("-", 3), operand2=operand2)
 
@@ -107,7 +107,7 @@ class TestInstruction(unittest.TestCase):
         self.assertEqual(result, [dest])
 
     def test_assignment_variable_operand(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operand1 = Token("a", 1)
         instr = Instruction(2, dest, operand1)
 
@@ -115,7 +115,7 @@ class TestInstruction(unittest.TestCase):
         self.assertEqual(result, [dest, operand1])
 
     def test_assignment_literal_operand(self):
-        dest     = Token("x", 0)
+        dest = Token("x", 0)
         operand1 = Token("42", 2)
         instr = Instruction(2, dest, operand1)
 
