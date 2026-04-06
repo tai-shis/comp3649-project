706ee3a7f130b8b8b52d9f278af3fe83b702abee
TyW04 <tywagner04@gmail.com>
2026-04-05 19:32:13 -0600

    rewrote to align with new code. All tests pass

--- haskell/Tests/InstructionTest.hs
+++ haskell/Tests/InstructionTest.hs
@@ -10,110 +10,126 @@ check name False = "FAIL: " ++ name
 
 -- Sample tokens
 destToken :: Token
-destToken = Token "x" Destination
+destToken = createToken "x" Destination
 
 varTokenA :: Token
-varTokenA = Token "a" Variable
+varTokenA = createToken "a" Variable
 
 varTokenB :: Token
-varTokenB = Token "b" Variable
+varTokenB = createToken "b" Variable
 
 addOpToken :: Token
-addOpToken = Token "+" Operator
+addOpToken = createToken "+" Operator
 
 negOpToken :: Token
-negOpToken = Token "-" Operator
+negOpToken = createToken "-" Operator
 
 litToken :: Token
-litToken = Token "42" Literal
+litToken = createToken "19" Literal
 
 -- Sample instructions
 binaryIns :: Instruction
-binaryIns = BinaryIns BinaryOperator destToken varTokenA addOpToken varTokenB
+binaryIns = createInstruction (destToken, varTokenA, addOpToken, varTokenB)
 
 unaryIns :: Instruction
-unaryIns = UnaryIns UnaryOperator destToken negOpToken varTokenA
+unaryIns = createInstruction (destToken, negOpToken, varTokenA)
 
 assignIns :: Instruction
-assignIns = AssignmentIns Assignment destToken litToken
+assignIns = createInstruction (destToken, litToken)
 
 main :: IO ()
 main = mapM_ putStrLn results
 
 results :: [String]
 results =
-    -- Construction tests
-    [ check "BinaryIns construction"
-        (binaryIns == BinaryIns BinaryOperator destToken varTokenA addOpToken varTokenB)
-    , check "UnaryIns construction"
-        (unaryIns == UnaryIns UnaryOperator destToken negOpToken varTokenA)
-    , check "AssignmentIns construction"
-        (assignIns == AssignmentIns Assignment destToken litToken)
+    [
+        -- createInstruction
+        check "createInstuction - BinaryProps" (binaryIns == createInstruction (destToken, varTokenA, addOpToken, varTokenB)),
+        check "createInstuction - UnaryProps" (unaryIns == createInstruction (destToken, negOpToken, varTokenA)),
+        check "createInstuction - AssignmentProps" (assignIns == createInstruction (destToken, litToken)),
+        
+        -- Show Instruction
+        check "Show Binary Instruction" (show binaryIns == "Binary Instruction: x : Destination = a : Variable + : Operator b : Variable"),
+        check "Show Unary Instruction" (show unaryIns == "Unary Instruction: x : Destination = - : Operator a : Variable"),
+        check "Show Assignment Instruction" (show assignIns == "Assignment Instruction: x : Destination = 19 : Literal"),
 
-    -- Show Instruction
-    , check "Show BinaryIns"
-        (show binaryIns
-            == "Binary Instruction: Token \"x\" Destination = Token \"a\" Variable Token \"+\" Operator Token \"b\" Variable")
-    , check "Show UnaryIns"
-        (show unaryIns
-            == "Unary Instruction: Token \"x\" Destination = Token \"-\" Operator Token \"a\" Variable")
-    , check "Show AssignmentIns"
-        (show assignIns
-            == "Assignment Instruction: Token \"x\" Destination = Token \"42\" Literal")
+        -- Eq Instruction
+        check "Eq Instruction - equal" (binaryIns == binaryIns),
+        check "Eq Instruction - unequal" (binaryIns /= assignIns),
+        
+        -- getDestination
+        check "getDestination - BinaryIns" (getDestination binaryIns == destToken),
+        check "getDestination - UnaryIns" (getDestination unaryIns == destToken),
+        check "getDestination - AssignIns" (getDestination assignIns == destToken),
+        
+        -- getVariables on BinaryIns
+        check "getVariables - BinaryIns" 
+            (getVariables (createInstruction (destToken, varTokenA, addOpToken, varTokenB)) 
+            == [varTokenA, varTokenB]),
+        check "getVariables BinaryIns - no variables" 
+            (getVariables (createInstruction (destToken, litToken, addOpToken, createToken "1" Literal))
+            == []),
+        check "getVariables BinaryIns - mixed tokens"
+            (getVariables (createInstruction (destToken, varTokenA, addOpToken, litToken))
+            == [varTokenA]),
 
-    -- Eq Instruction
-    , check "Eq Instruction - equal"
-        (binaryIns == binaryIns)
-    , check "Eq Instruction - unequal"
-        (binaryIns /= assignIns)
+        -- getVariables on UnaryIns
+        check "getVariables UnaryIns - variable operand"
+            (getVariables (createInstruction (destToken, negOpToken, varTokenA))
+            == [varTokenA]),
+        check "getVariables UnaryIns - no variables"
+            (getVariables (createInstruction (destToken, negOpToken, litToken))
+            == []),
 
-    -- getVariables on BinaryIns
-    , check "getVariables BinaryIns - all variables"
-        (getVariables (BinaryIns BinaryOperator (Token "x" Variable) varTokenA addOpToken varTokenB)
-            == [Token "x" Variable, varTokenA, varTokenB])
-    , check "getVariables BinaryIns - no variables"
-        (getVariables (BinaryIns BinaryOperator destToken litToken addOpToken (Token "1" Literal))
-            == [])
-    , check "getVariables BinaryIns - mixed tokens"
-        (getVariables (BinaryIns BinaryOperator destToken varTokenA addOpToken litToken)
-            == [varTokenA])
+        -- getVariables on AssignmentIns
+        check "getVariables AssignmentIns - variable operand"
+            (getVariables (createInstruction (destToken, varTokenA))
+            == [varTokenA]),
+        check "getVariables AssignmentIns - no variables"
+            (getVariables (createInstruction (destToken, litToken))
+            == []),
 
-    -- getVariables on UnaryIns
-    , check "getVariables UnaryIns - with variable"
-        (getVariables (UnaryIns UnaryOperator destToken negOpToken varTokenA)
-            == [varTokenA])
-    , check "getVariables UnaryIns - no variables"
-        (getVariables (UnaryIns UnaryOperator destToken negOpToken litToken)
-            == [])
+        -- emptyInstruction
+        check "emptyInstructions - empty instructions list"
+            (getInstructions emptyInstructions == []),
+        check "emptyInstructions - empty live variables list"
+            (getLiveVariables emptyInstructions == []),
 
-    -- getVariables on AssignmentIns
-    , check "getVariables AssignmentIns - with variables"
-        (getVariables (AssignmentIns Assignment (Token "x" Variable) varTokenA)
-            == [Token "x" Variable, varTokenA])
-    , check "getVariables AssignmentIns - no variables"
-        (getVariables (AssignmentIns Assignment destToken litToken)
-            == [])
+        -- fromArraysInstructions
+        check "fromArraysInstructions - stores instructions"
+            (getInstructions (fromArraysInstructions [assignIns] []) == [assignIns]),
+        check "fromArraysInstructions - stores live variables"
+            (getLiveVariables (fromArraysInstructions [] ["a", "b"]) == ["a", "b"]),
+        check "fromArraysInstructions - stores both"
+        (getInstructions (fromArraysInstructions [assignIns] ["a"]) == [assignIns]
+            && getLiveVariables (fromArraysInstructions [assignIns] ["a"]) == ["a"]),
 
-    -- showInstructions
-    , check "showInstructions - empty list"
-        (showInstructions [] == "Instructions: \n")
-    , check "showInstructions - single instruction"
-        (showInstructions [assignIns]
-            == "Instructions: \nAssignment Instruction: Token \"x\" Destination = Token \"42\" Literal\n")
-    , check "showInstructions - multiple instructions"
-        (showInstructions [assignIns, AssignmentIns Assignment (Token "y" Destination) (Token "1" Literal)]
-            == "Instructions: \nAssignment Instruction: Token \"x\" Destination = Token \"42\" Literal\nAssignment Instruction: Token \"y\" Destination = Token \"1\" Literal\n")
+        -- getInstructions
+        check "getInstructions - empty" (getInstructions emptyInstructions == []),
+        check "getInstructions - populated"
+        (getInstructions (fromArraysInstructions [assignIns, binaryIns] []) == [assignIns, binaryIns]),
 
-    -- showLiveVars
-    , check "showLiveVars - empty list"
-        (showLiveVars [] == "Live: ")
-    , check "showLiveVars - single variable"
-        (showLiveVars [LiveVar "x"] == "Live: x,")
-    , check "showLiveVars - multiple variables"
-        (showLiveVars [LiveVar "x", LiveVar "y"] == "Live: x,y,")
+        -- getLiveVariables
+        check "getLiveVariables - empty" (getLiveVariables emptyInstructions == []),
+        check "getLiveVariables - populated"
+            (getLiveVariables (fromArraysInstructions [] ["a", "b"]) == ["a", "b"]),
 
-    -- Show Instructions (uses the Instruction constructor from data Instructions)
-    , check "Show Instructions"
-    (show (Instructions ([assignIns], [LiveVar "x"]))
-        == showInstructions [assignIns] ++ showLiveVars [LiveVar "x"])
+        -- showInstructions
+        check "showInstructions - empty list" (showInstructions [] == "Instructions: \n"),
+        check "showInstructions - single instruction"
+            (showInstructions [assignIns]
+            == "Instructions: \nAssignment Instruction: x : Destination = 19 : Literal\n"),
+        check "showInstructions - multiple instructions"
+            (showInstructions [assignIns, assignIns]
+            == "Instructions: \nAssignment Instruction: x : Destination = 19 : Literal\nAssignment Instruction: x : Destination = 19 : Literal\n"),
+            
+        -- showLiveVars
+        check "showLiveVars - empty list" (showLiveVars [] == "Live: "),
+        check "showLiveVars - single variable" (showLiveVars ["x"] == "Live: \"x\""),
+        check "showLiveVars - multiple variables" (showLiveVars ["x", "y"] == "Live: \"x\", \"y\""),
+
+        -- Show Instructions
+        check "Show Instructions"
+        (show (fromArraysInstructions [assignIns] ["x"])
+            == showInstructions [assignIns] ++ showLiveVars ["x"])
     ]
