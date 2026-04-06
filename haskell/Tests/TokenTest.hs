8da614b24e89a3807e3aec487fbfafe6f006035d
TyW04 <tywagner04@gmail.com>
2026-04-05 18:12:07 -0600

    Rewrote tests to match module code changes

--- haskell/Tests/TokenTest.hs
+++ haskell/Tests/TokenTest.hs
@@ -1,41 +1,56 @@
 module TokenTest where
-
 import Input.Token
 
--- Helper to report pass/fail
 check :: String -> Bool -> String
-check name True  = "PASS: " ++ name
-check name False = "FAIL: " ++ name
+check name True = "PASS: " ++ name
+check name False = "FAIL: " ++ name 
 
-main :: IO ()
-main = mapM_ putStrLn results
+testToken :: IO ()
+testToken = mapM_ putStrLn results
 
 results :: [String]
-results =
-    -- Construction tests (verify Eq holds against expected value)
-    [ check "Token construction - variable"    (Token "x" Variable    == Token "x" Variable)
-    , check "Token construction - literal"     (Token "42" Literal    == Token "42" Literal)
-    , check "Token construction - operator"    (Token "+" Operator    == Token "+" Operator)
-    , check "Token construction - destination" (Token "dest" Destination == Token "dest" Destination)
-    , check "Token construction - EOF"         (Token "" EOF          == Token "" EOF)
-
-    -- Show TokenType
-    , check "Show TokenType - Variable"  (show Variable  == "Variable")
-    , check "Show TokenType - Operator"  (show Operator  == "Operator")
-    , check "Show TokenType - Newline"   (show Newline   == "Newline")
-    , check "Show TokenType - Live" (show Live == "Live")
-    , check "Show TokenType - EOF"       (show EOF       == "EOF")
-
-    -- Show Token
-    , check "Show Token" (show (Token "x" Variable) == "Token \"x\" Variable")
-
-    -- Eq TokenType
-    , check "Eq TokenType - equal types"   (Variable == Variable)
-    , check "Eq TokenType - unequal types" (Variable /= Literal)
-
-    -- Eq Token
-    , check "Eq Token - equal tokens"      (Token "x" Variable == Token "x" Variable)
-    , check "Eq Token - different string"  (Token "x" Variable /= Token "y" Variable)
-    , check "Eq Token - different type"    (Token "x" Variable /= Token "x" Literal)
-    , check "Eq Token - both fields differ" (Token "x" Variable /= Token "42" Literal)
+results = 
+    [
+        -- createToken
+        check "createToken - getValue returns correct value" (getValue (createToken "x" Destination) == "x"),
+        check "createToken - getType returns correct type" (getType (createToken "x" Destination) == Destination),
+        check "createToken - Variable token" (getType (createToken "a" Variable) == Variable),
+        check "createToken - Literal token" (getType (createToken "42" Literal) == Literal),
+        check "createToken - Operator token" (getType (createToken "+" Operator) == Operator),
+        check "createToken - Equals token" (getType (createToken "=" Equals) == Equals),
+        check "createToken - Live token" (getType (createToken "live:" Live) == Live),
+        check "createToken - LiveSymbol token" (getType (createToken "a," LiveSymbol) == LiveSymbol),
+        check "createToken - Newline token" (getType (createToken "\n" Newline) == Newline),
+        check "createToken - EOF token" (getType (createToken "" EOF) == EOF),
+        -- getValue
+        check "getValue - variable name" (getValue (createToken "abc" Variable) == "abc"),
+        check "getValue - empty string" (getValue (createToken "" EOF) == ""),
+        check "getValue - operator symbol" (getValue (createToken "-" Operator) == "-"),
+        -- getType
+        check "getType - Destination" (getType (createToken "x" Destination) == Destination),
+        check "getType - Literal" (getType (createToken "99" Literal) == Literal),
+        check "getType - EOF" (getType (createToken "" EOF) == EOF),
+        -- Show TokenType
+        check "Show TokenType - Destination" (show Destination == "Destination"),
+        check "Show TokenType - Variable" (show Variable == "Variable"),
+        check "Show TokenType - Literal" (show Literal == "Literal"),
+        check "Show TokenType - Operator" (show Operator == "Operator"),
+        check "Show TokenType - Equals" (show Equals == "Equals"),
+        check "Show TokenType - Live" (show Live == "Live"),
+        check "Show TokenType - LiveSymbol" (show LiveSymbol == "LiveSymbol"),
+        check "Show TokenType - Newline" (show Newline == "Newline"),
+        check "Show TokenType - EOF" (show EOF == "EOF"),
+        -- Show Token
+        check "Show Token - Variable" (show (createToken "a" Variable) == "a : Variable"),
+        check "Show Token - Literal" (show (createToken "42" Literal) == "42 : Literal"),
+        check "Show Literal - Operator" (show (createToken "+" Operator) == "+ : Operator"),
+        check "Show EOF - Operator" (show (createToken "" EOF) == " : EOF"),
+        -- Eq Token
+        check "Eq Token - equal tokens" (createToken "x" Variable == createToken "x" Variable),
+        check "Eq Token - different values" (createToken "x" Variable /= createToken "y" Variable),
+        check "Eq Token - different types" (createToken "x" Destination /= createToken "x" Variable),
+        check "Eq Token - value & type unequal" (createToken "x" Destination /= createToken "10" Literal),
+        -- Eq TokenType
+        check "Eq TokenType - equal types" (Variable == Variable),
+        check "Eq TokenType - unequal types" (Variable /= Destination)
     ]
