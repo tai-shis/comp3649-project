02191f0f29239bd745ee11e5f0a2a6e7c9bc9502
TyW04 <tywagner04@gmail.com>
2026-04-06 12:38:06 -0600

    Added tests for determineLiveness. Warnings present for possible error on empty list, but tests pass as expected.

--- haskell/Tests/LivenessTest.hs
+++ haskell/Tests/LivenessTest.hs
@@ -94,7 +94,40 @@ results =
         -- showLivenessStates
         check "showLivenessStates - empty list" (showLivenessStates [] == ""),
         check "showLivenessStates - single line" (showLivenessStates [[L.Defined "x", L.Live "a"]] == "x: defined, a: live\n"),
-        check "showLivenessStates - multiple lines" (showLivenessStates [[L.Defined "x"], [L.Live "a"]] == "x: defined\na: live\n")
+        check "showLivenessStates - multiple lines" (showLivenessStates [[L.Defined "x"], [L.Live "a"]] == "x: defined\na: live\n"),
 
         -- determineLiveness
+        check "determineLiveness - empty instructions" (determineLiveness emptyInstructions == [[]]),
+        check "determineLiveness - single assignment, operand live"
+        -- x = a, live: a
+        -- line 0: x defined, a live. line 1 (live section): a live
+            (let liveness = determineLiveness (fromArraysInstructions [assignXA] ["a"])
+                in isDefined (head (filter isDefined (liveness !! 0)))
+                && isLive (head (filter isLive (liveness !! 0)))
+                && getLivenessName (head (filter isDefined (liveness !! 0))) == "x"
+                && getLivenessName (head (filter isLive (liveness !! 0))) == "a"),
+        check "determineLiveness - single assignment, no live vars"
+        -- x = a, no live vars. a should be unlive
+            (let liveness = determineLiveness (fromArraysInstructions [assignXA] [])
+            in any (\s -> isUnlive s && getLivenessName s == "a") (liveness !! 0)),
+        check "determineLiveness - single binary instruction"
+        -- x = a + b, live: a, b. x defined, a and b live
+            (let liveness = determineLiveness (fromArraysInstructions [binaryXAB] ["a", "b"])
+                 line0 = liveness !! 0
+                in any (\s -> isDefined s && getLivenessName s == "x") line0
+                    && any (\s -> isLive s && getLivenessName s == "a") line0
+                    && any (\s -> isLive s && getLivenessName s == "b") line0),
+        check "determineLiveness - variable defined then used"
+            -- x = a, y = x, live: x. x defined on line 0, live on line 1
+            (let liveness = determineLiveness (fromArraysInstructions [assignXA, assignYX] ["x"])
+                in any (\s -> isDefined s && getLivenessName s == "x") (liveness !! 0)
+                && any (\s -> isLive s && getLivenessName s == "x") (liveness !! 1)),
+        check "determineLiveness - variable unlive after last use"
+        -- x = a, y = b, live: b. a is unlive on line 0
+        (let liveness = determineLiveness (fromArraysInstructions [assignXA, assignYB] ["b"])
+            in any (\s -> isUnlive s && getLivenessName s == "a") (liveness !! 0)),
+        check "determineLiveness - result length"
+        -- 3 instructions + 1 live section = 4 entries
+        (let liveness = determineLiveness (fromArraysInstructions [assignXA, assignYB, assignXLit] [])
+         in length liveness == 4)
     ]
